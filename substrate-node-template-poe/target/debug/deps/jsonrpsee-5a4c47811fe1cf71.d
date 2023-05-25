1d9dccd7ab234509935f62d67e326bdc8c18a4cc
heart <2105026385@qq.com>
2023-05-25 09:25:02 +0800

    更改自己转移存证给自己的测试代码和注释

---
+++ substrate-node-template-poe/target/debug/deps/jsonrpsee-5a4c47811fe1cf71.d
@@ -0,0 +1,6 @@
+C:\Users\Heart\Desktop\新建文件夹\Substrate\Substrate\substrate-node-template-poe\target\debug\deps\jsonrpsee-5a4c47811fe1cf71.rmeta: D:\rust\rust\.cargo\registry\src\mirrors.tuna.tsinghua.edu.cn-df7c3c540f42cdbd\jsonrpsee-0.16.2\src\lib.rs D:\rust\rust\.cargo\registry\src\mirrors.tuna.tsinghua.edu.cn-df7c3c540f42cdbd\jsonrpsee-0.16.2\src\macros.rs
+
+C:\Users\Heart\Desktop\新建文件夹\Substrate\Substrate\substrate-node-template-poe\target\debug\deps\jsonrpsee-5a4c47811fe1cf71.d: D:\rust\rust\.cargo\registry\src\mirrors.tuna.tsinghua.edu.cn-df7c3c540f42cdbd\jsonrpsee-0.16.2\src\lib.rs D:\rust\rust\.cargo\registry\src\mirrors.tuna.tsinghua.edu.cn-df7c3c540f42cdbd\jsonrpsee-0.16.2\src\macros.rs
+
+D:\rust\rust\.cargo\registry\src\mirrors.tuna.tsinghua.edu.cn-df7c3c540f42cdbd\jsonrpsee-0.16.2\src\lib.rs:
+D:\rust\rust\.cargo\registry\src\mirrors.tuna.tsinghua.edu.cn-df7c3c540f42cdbd\jsonrpsee-0.16.2\src\macros.rs:
---
+++ substrate-node-template-poe/target/debug/deps/sc_peerset-ff5d72e088f5d5a9.d
@@ -0,0 +1,6 @@
+C:\Users\Heart\Desktop\新建文件夹\Substrate\Substrate\substrate-node-template-poe\target\debug\deps\sc_peerset-ff5d72e088f5d5a9.rmeta: D:\rust\rust\.cargo\git\checkouts\substrate-7e08433d4c370a21\ba87188\client\peerset\src\lib.rs D:\rust\rust\.cargo\git\checkouts\substrate-7e08433d4c370a21\ba87188\client\peerset\src\peersstate.rs
+
+C:\Users\Heart\Desktop\新建文件夹\Substrate\Substrate\substrate-node-template-poe\target\debug\deps\sc_peerset-ff5d72e088f5d5a9.d: D:\rust\rust\.cargo\git\checkouts\substrate-7e08433d4c370a21\ba87188\client\peerset\src\lib.rs D:\rust\rust\.cargo\git\checkouts\substrate-7e08433d4c370a21\ba87188\client\peerset\src\peersstate.rs
+
+D:\rust\rust\.cargo\git\checkouts\substrate-7e08433d4c370a21\ba87188\client\peerset\src\lib.rs:
+D:\rust\rust\.cargo\git\checkouts\substrate-7e08433d4c370a21\ba87188\client\peerset\src\peersstate.rs:
